5a2fdde274c8cca7523e68c5b81cfb23a256912b
naluwn <street.baller06@gmail.com>
2021-10-19 16:35:36 +0800

    feat: add function of edit company info

--- routes/modules/position.js
+++ routes/modules/position.js
@@ -6,7 +6,7 @@ const sql = require('mssql')
 
 router.put('/:position_id', (req, res) => {
     const { position_id } = req.params
-    const { name, des } = req.body
+    const { POSITION_NAME, POSITION_DES } = req.body
     const cpyNo = res.locals.cpyNo
 
     sql.connect(db, (err) => {
@@ -15,24 +15,33 @@ router.put('/:position_id', (req, res) => {
         const request = new sql.Request()
         const errors = []
 
-        request.query(`select * from BOTFRONT_TEST_POSITION where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
-            if(err){
-                sql.close()
-                errors.push({message: '查無此職缺資訊!'})
-                return res.render('position', {errors})
-            }
-        })
-
-        request.input('name', sql.NVarChar(40), name)
-        .input('des', sql.NVarChar(1000), des)
-        .query(`update BOTFRONT_TEST_POSITION set POSITION_NAME = @name, POSITION_DES = @des where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
+        request.query(`select * 
+        from BOTFRONT_TEST_POSITION 
+        where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
             if(err){
                 sql.close()
                 console.log(err)
-                return res.send(err)
             }
-            sql.close()
-           return res.redirect('/position')
+            
+            result = result.recordset[0]
+            if(!result) errors.push({message: '職缺錯誤，請重新操作!'})
+            if(errors.length){
+                return res.render('edit_position', {errors, result: {POSITION_NAME, POSITION_DES}})
+            }else{
+                request.input('name', sql.NVarChar(40), name)
+                .input('des', sql.NVarChar(1000), des)
+                .query(`update BOTFRONT_TEST_POSITION 
+                set POSITION_NAME = @name, POSITION_DES = @des 
+                where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
+                    if(err){
+                        sql.close()
+                        console.log(err)
+                        return res.send(err)
+                    }
+                    sql.close()
+                    return res.redirect('/position')
+                })
+            }
         })
     })
 })
@@ -47,7 +56,9 @@ router.get('/:position_id/edit', (req, res) => {
         const request = new sql.Request()
         const errors = []
 
-        request.query(`select * from BOTFRONT_TEST_POSITION where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
+        request.query(`select * 
+        from BOTFRONT_TEST_POSITION 
+        where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
             if(err){
                 sql.close()
                 console.log(err)
@@ -57,14 +68,25 @@ router.get('/:position_id/edit', (req, res) => {
             result = result.recordset[0]
             // console.log(result)
 
-            if(!result){
-                errors.push({message:'查無此職缺資料!'})
-                sql.close()
-                console.log(err)
-                return res.render('edit_position', {errors})
+            if(!result) errors.push({message:'查無此職缺資料!'})
+            if(errors.length){
+                request.query(`select * 
+                from BOTFRONT_TEST_POSITION 
+                where CPYID=${res.locals.cpyNo}`, (err, result) => {
+                    if(err){
+                        sql.close()
+                        console.log(err)
+                        return res.send(err)
+                    }
+                    
+                    const positionResult = result.recordset
+                    // console.log(positionResult)
+                    sql.close()
+                    return res.render('position', {positionResult, errors})
+                })
+            }else{
+                return res.render('edit_position', {result})
             }
-           
-           return res.render('edit_position', {result})
         })
     })
 })
@@ -79,7 +101,9 @@ router.delete('/:position_id', (req, res) => {
         const request = new sql.Request()
         const errors = []
 
-        request.query(`select * from BOTFRONT_TEST_POSITION where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
+        request.query(`select * 
+        from BOTFRONT_TEST_POSITION 
+        where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
             if(err){
                 sql.close()
                 console.log(err)
@@ -90,7 +114,9 @@ router.delete('/:position_id', (req, res) => {
             if(!result) errors.push({message: '查無此職缺資訊!'})
                 
             if(errors.length){
-                request.query(`select * from BOTFRONT_TEST_POSITION where CPYID=${res.locals.cpyNo}`, (err, result) => {
+                request.query(`select * 
+                from BOTFRONT_TEST_POSITION 
+                where CPYID=${res.locals.cpyNo}`, (err, result) => {
                     if(err){
                         sql.close()
                         console.log(err)
@@ -103,7 +129,9 @@ router.delete('/:position_id', (req, res) => {
                     return res.render('position', {positionResult, errors})
                 })
             }else{
-                request.query(`delete from BOTFRONT_TEST_POSITION where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
+                request.query(`delete 
+                from BOTFRONT_TEST_POSITION 
+                where POSITION_ID = ${position_id} and CPYID = ${cpyNo}`, (err, result) => {
                     if(err){
                         sql.close()
                         console.log(err)
@@ -137,7 +165,8 @@ router.post('/', (req, res) => {
         request.input('name', sql.NVarChar(40), name)
         .input('des', sql.NVarChar(1000), des)
         .input('cpyNo', sql.Int, cpyNo)
-        .query('insert into BOTFRONT_TEST_POSITION (POSITION_NAME, POSITION_DES, CPYID) values (@name, @des, @cpyNo)', (err, result) => {
+        .query(`insert into BOTFRONT_TEST_POSITION (POSITION_NAME, POSITION_DES, CPYID) 
+        values (@name, @des, @cpyNo)`, (err, result) => {
             if(err){
                 sql.close()
                 console.log(err)
@@ -158,7 +187,9 @@ router.get('/', (req, res) => {
         if(err) console.log(err)
 
         const request = new sql.Request()
-        request.query(`select * from BOTFRONT_TEST_POSITION where CPYID=${res.locals.cpyNo}`, (err, result) => {
+        request.query(`select * 
+        from BOTFRONT_TEST_POSITION 
+        where CPYID=${res.locals.cpyNo}`, (err, result) => {
             if(err){
                 sql.close()
                 console.log(err)
