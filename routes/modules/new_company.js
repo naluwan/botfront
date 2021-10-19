e1a8de25714ea596655076f4fa12405667c8798d
naluwn <street.baller06@gmail.com>
2021-10-19 12:23:52 +0800

    feat: add function of add company info

--- routes/modules/new_company.js
+++ routes/modules/new_company.js
@@ -4,6 +4,50 @@ const router = express.Router()
 const db = require('../../config/db')
 const sql = require('mssql')
 
+router.post('/', (req, res) => {
+    const cpyNo = res.locals.cpyNo
+    const {category, des} = req.body
+    const categorySelected = category
+    // console.log(req.body)
+    sql.connect(db, (err) => {
+        if(err) console.log(err)
+        
+        const request = new sql.Request()
+        const errors = []
+
+        if(!category || category == '' || !des){
+            errors.push({message: '所有欄位都是必填的!'})
+        }
+
+        if(errors.length){
+            request.query(`select INFO_NO, INFO_NAME from BOTFRONT_ALL_COMPANY_INFO a where not exists (select * from BOTFRONT_TEST_COMPANY_INFO b where b.INFO_NO = a.INFO_NO and CPYID = ${cpyNo})`, (err, result) => {
+                if(err){
+                    sql.close()
+                    console.log(err)
+                    return res.send(err)
+                }
+    
+                const category = result.recordset
+                sql.close()
+                return res.render('new_company', {errors, des, category})
+            })
+        }
+        request.input('cpyNo', sql.Int, cpyNo)
+        .input('info_no', sql.Int, category)
+        .input('des', sql.NVarChar(2000), des)
+        .query(`insert into BOTFRONT_TEST_COMPANY_INFO (CPYID, INFO_NO, INFO_DES) values (@cpyNo, @info_no, @des)`, (err, result) => {
+            if(err){
+                sql.close()
+                console.log(err)
+                return res.send(err)
+            }
+
+            sql.close()
+            return res.redirect('/company')
+        })
+    })
+})                              
+
 router.get('/new', (req, res) => {
     const cpyNo = res.locals.cpyNo
     sql.connect(db, (err) => {
