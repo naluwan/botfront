1241dbd11dcdab322184952646a33cb089e9f022
naluwn <street.baller06@gmail.com>
2021-10-21 16:40:24 +0800

    fix: debug for middleware/getCompanyName.js

--- middleware/getCompanyName.js
+++ middleware/getCompanyName.js
@@ -1,28 +1,22 @@
 exports.flashMessage = function (req, res, next) {
-    const db = require('../config/db')
-    const sql = require('mssql')
+	const sql = require('mssql')
+	const pool = require('../../config/connectPool')
 
-    sql.connect(db, (err) => {
-    if(err)return res.send(err)
-    
-    const request = new sql.Request()
-    request.query(`select CPY_NAME from BOTFRONT_COMPANY where CPY_NO=${res.locals.cpyNo}`, (err, result) => {
-        if(err){
-        sql.close()
-        console.log(err)
-        return res.send(err)
-        }
-        // console.log(result.recordset[0])
-        // 檢查資料庫是否有值
-        if(!result.recordset[0]) {
-            res.locals.companyName = ''
-            return next()
-        }
-        result = result.recordset[0].CPY_NAME
-        sql.close()
-        // console.log(result)
-        res.locals.companyName = result
-        next();
-        })
-    })
+	const request = new sql.Request(pool)
+	request.query(`select CPY_NAME from BOTFRONT_COMPANY where CPY_NO=${res.locals.cpyNo}`, (err, result) => {
+		if(err){
+		console.log(err)
+		return
+		}
+		// console.log(result.recordset[0])
+		// 檢查資料庫是否有值
+		if(!result.recordset[0]) {
+		res.locals.companyName = ''
+		return next()
+		}
+		result = result.recordset[0].CPY_NAME
+		// console.log(result)
+		res.locals.companyName = result
+		next();
+	})
 }
