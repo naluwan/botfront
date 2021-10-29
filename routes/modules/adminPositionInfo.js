6e30a55f4e87894650002cd0b15fc156bce892bb
naluwn <street.baller06@gmail.com>
2021-10-29 14:41:37 +0800

    feat: add admin delete func and admin add new position func

--- routes/modules/adminPositionInfo.js
+++ routes/modules/adminPositionInfo.js
@@ -6,6 +6,60 @@ const {isAdmin} = require('../../middleware/auth')
 
 const sql = require('mssql')
 const pool = require('../../config/connectPool')
+const { query } = require('express')
+
+router.delete('/:POSITION_ID', (req, res) => {
+  const {POSITION_ID} = req.params
+
+  const request = new sql.Request(pool)
+
+  request.query(`select * 
+  from BOTFRONT_ALL_POSITION
+  where POSITION_ID = ${POSITION_ID}`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    result = result.recordset[0]
+    if(!result){
+      req.flash('error', '查無此職缺類別，請重新嘗試!')
+      return res.redirect('/adminPositionInfo')
+    }
+    request.query(`delete
+    from BOTFRONT_ALL_POSITION
+    where POSITION_ID = ${POSITION_ID}`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      req.flash('success_msg', '職缺類別刪除成功!!')
+      res.redirect('/adminPositionInfo')
+    })
+  })
+})
+
+router.post('/', (req, res) => {
+  const {industry_no, position_name, position_entity_name} = req.body
+
+  if(!industry_no || !position_name || !position_entity_name){
+    req.flash('error', '所有欄位都是必填的!!')
+    return res.redirect('/adminPositionInfo/new')
+  }
+
+  const request = new sql.Request(pool)
+  request.input('industry_no', sql.Int, industry_no)
+  .input('position_name', sql.NVarChar(200), position_name)
+  .input('position_entity_name', sql.NVarChar(200), position_entity_name)
+  .query(`insert into BOTFRONT_ALL_POSITION (INDUSTRY_NO, POSITION_NAME, POSITION_ENTITY_NAME)
+  values (@industry_no, @position_name, @position_entity_name)`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    req.flash('success_msg', '新增成功!!')
+    res.redirect('/adminPositionInfo')
+  })
+})
 
 router.get('/new', (req, res) => {
   const request = new sql.Request(pool)
