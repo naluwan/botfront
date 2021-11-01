65ebe3cd820dbdef0e653f4c64f58386c47a3c81
naluwn <street.baller06@gmail.com>
2021-11-01 16:16:36 +0800

    feat: add new_adminLeaveInfo.hbs and add new type of Leave func

--- routes/modules/adminLeaveInfo.js
+++ routes/modules/adminLeaveInfo.js
@@ -9,6 +9,32 @@ const pool = require('../../config/connectPool')
 const { query } = require('express')
 const request = new sql.Request(pool)
 
+router.post('/', (req, res) => {
+  const {leave_name, leave_entity_name} = req.body
+  const errors = []
+
+  if(!leave_name || !leave_entity_name){
+    errors.push({message: '所有欄位都是必填的!!'})
+    return res.render('new_adminLeaveInfo', {leave_name, leave_entity_name, errors})
+  }
+
+  request.input('leave_name', sql.NVarChar(20), leave_name)
+  .input('leave_entity_name', sql.NVarChar(50), leave_entity_name)
+  .query(`insert into BOTFRONT_ALL_LEAVE (LEAVE_NAME, LEAVE_ENTITY_NAME)
+  values (@leave_name, @leave_entity_name)`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    req.flash('success_msg', '公司假別新增成功!!')
+    res.redirect('/adminLeaveInfo')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('new_adminLeaveInfo')
+})
+
 router.get('/', (req, res) => {
   const {search} = req.query
   const warning = []
