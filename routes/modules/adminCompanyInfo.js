5ca62d132a4dc83af2b8488f5554be9057e9822b
naluwn <street.baller06@gmail.com>
2021-11-01 14:47:53 +0800

    feat: 新增管理員刪除公司資訊類別和新增公司資訊類別的功能

--- routes/modules/adminCompanyInfo.js
+++ routes/modules/adminCompanyInfo.js
@@ -6,7 +6,62 @@ const {isAdmin} = require('../../middleware/auth')
 
 const sql = require('mssql')
 const pool = require('../../config/connectPool')
+const { query } = require('express')
 const request = new sql.Request(pool)
+
+router.delete('/:info_id', (req, res) => {
+  const {info_id} = req.params
+
+  request.query(`select *
+  from BOTFRONT_ALL_COMPANY_INFO
+  where INFO_ID = ${info_id}`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    result = result.recordset[0]
+    if(!result){
+      req.flash('error', '找不到此公司資訊，請重新嘗試!!')
+      return res.redirect('/adminCompanyInfo')
+    }
+    request.query(`delete 
+    from BOTFRONT_ALL_COMPANY_INFO
+    where INFO_ID = ${info_id}`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      req.flash('success_msg', '已成功刪除公司資訊!!')
+      res.redirect('/adminCOmpanyInfo')
+    })
+  })
+})
+
+router.post('/', (req, res) => {
+  const {info_name, info_entity_name} = req.body
+  const errors = []
+  
+  if(!info_name || !info_entity_name){
+    errors.push({message: '所有欄位都是必填的!!'})
+    return res.render('new_adminCompanyInfo', {info_name, info_entity_name, errors})
+  }
+
+  request.input('info_name', sql.NVarChar(200), info_name)
+  .input('info_entity_name', sql.NVarChar(200), info_entity_name)
+  .query(`insert into BOTFRONT_ALL_COMPANY_INFO (INFO_NAME, INFO_ENTITY_NAME)
+  values (@info_name, @info_entity_name)`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    req.flash('success_msg', '新增公司資訊成功!!')
+    res.redirect('/adminCompanyInfo')
+  })
+})
+
+router.get('/new', (req, res) => {
+  res.render('new_adminCompanyInfo')
+})
 
 router.get('/', (req, res) => {
   const warning = []
