4e3df863fb61700ec3f2fff47c22cb84abda6892
naluwn <street.baller06@gmail.com>
2021-11-05 15:08:51 +0800

    feat: add the func of Admin adding new company info, and optimizing new_adminSearch.hbs

--- routes/modules/adminSearch.js
+++ routes/modules/adminSearch.js
@@ -8,6 +8,263 @@ const sql = require('mssql')
 const pool = require('../../config/connectPool')
 const { query } = require('express')
 
+router.post('/', (req, res) => {
+  const {companyFilter, tableFilter, industry_select, adminSearch_des, admin_select } = req.body
+  const errors = []
+
+  const request = new sql.Request(pool)
+
+  // 抓取公司
+  request.query(`select *
+  from BOTFRONT_USERS_INFO`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const adminCompany = result.recordset
+
+    // 驗證欄位是否都有值
+    if(!companyFilter || !tableFilter || !industry_select || !adminSearch_des || !admin_select){
+      req.flash('error', '所有欄位都是必填的!!')
+      return res.redirect('/adminSearch/new')
+    }
+
+    // 因資料庫table取名有些不同，所以需要判別要使用哪張table
+    // tableFilter選擇公司資訊
+    if(tableFilter == 'COMPANY'){
+      // 驗證要輸入的公司資訊類別有沒有在公司資訊類別中
+      request.query(`select *
+      from BOTFRONT_ALL_COMPANY_INFO
+      where INFO_ID = ${admin_select}`, (err, result) => {
+        if(err){
+          console.log(err)
+          return
+        }
+        const infoCheck = result.recordset
+        if(infoCheck.length == 0){
+          req.flash('error', '無此新增項目，請重新嘗試!!')
+          return res.redirect('/adminSearch/new')
+        }else{
+          // 驗證要新增的公司資訊類別是否已經新增過
+          request.query(`select * 
+          from BOTFRONT_COMPANY_INFO a
+          left join BOTFRONT_ALL_COMPANY_INFO b
+          on a.INFO_NO = b.INFO_ID
+          where a.CPY_NO = ${companyFilter} and a.INFO_NO = ${admin_select}`, (err, result) => {
+            if(err){
+              console.log(err)
+              return
+            }
+            const infoAdded = result.recordset
+            // 如果沒有新增過，結果會是一個[]
+            if(infoAdded.length == 0){
+              request.input('cpy_no', sql.Int, companyFilter)
+              .input('info_no', sql.Int, admin_select)
+              .input('des', sql.NVarChar(2000), adminSearch_des)
+              .query(`insert into BOTFRONT_COMPANY_INFO (CPY_NO, INFO_NO, INFO_DES)
+              values (@cpy_no, @info_no, @des)`, (err, result) => {
+                if(err){
+                  console.log(err)
+                  return
+                }
+                req.flash('success_msg', '回覆新增成功!!')
+                // return res.render(`adminSearch`, {adminCompany, companyFilter, tableFilter})
+                return res.redirect(`/adminSearch/filter?companyFilter=${companyFilter}&tableFilter=${tableFilter}&search=`)
+              })
+            }else{
+              req.flash('error', '無法新增資料，請確認此公司是否已新增過此類別資料，如已新增過請使用編輯功能修改內容!!')
+              return res.redirect('/adminSearch/new')
+            }
+          })
+        }
+      })
+    }
+
+    // tableFilter選擇職缺
+    if(tableFilter == 'POSITION'){
+      // 驗證要新增的職缺類別是否有在職缺類別中
+      request.query(`select * 
+      from BOTFRONT_ALL_POSITION
+      where POSITION_ID = ${admin_select} and INDUSTRY_NO = ${industry_select}`, (err, result) => {
+        if(err){
+          console.log(err)
+          return
+        }
+        const positionCheck = result.recordset
+        if(positionCheck.length == 0){
+          req.flash('error', '無此新增項目，請重新嘗試!!')
+          return res.redirect('/adminSearch/new')
+        }else{
+          // 驗證要新增的職缺類別是否已經新增過
+          request.query(`select * 
+          from BOTFRONT_POSITION_INFO
+          where CPY_NO = ${companyFilter} 
+          and INDUSTRY_NO = ${industry_select} 
+          and POSITION_NO = ${admin_select}`, (err, result) => {
+            if(err){
+              console.log(err)
+              return
+            }
+            const positionAdded = result.recordset
+            // 如果沒有新增過，結果會是[]
+            if(positionAdded.length == 0){
+              request.input('cpy_no', sql.Int, companyFilter)
+              .input('industry_no', sql.Int, industry_select)
+              .input('position_no', sql.Int, admin_select)
+              .input('des', sql.NVarChar(2000), adminSearch_des)
+              .query(`insert into BOTFRONT_POSITION_INFO (CPY_NO, INDUSTRY_NO, POSITION_NO, POSITION_DES)
+              values (@cpy_no, @industry_no, @position_no, @des)`, (err, result) => {
+                if(err){
+                  console.log(err)
+                  return
+                }
+                req.flash('success_msg', '回覆新增成功!!')
+                return res.redirect(`/adminSearch/filter?companyFilter=${companyFilter}&tableFilter=${tableFilter}&search=`)
+              })
+            }else{
+              req.flash('error', '無法新增資料，請確認此公司是否已新增過此類別資料，如已新增過請使用編輯功能修改內容!!')
+              return res.redirect('/adminSearch/new')
+            }
+          })
+        }
+      })
+    }
+
+    if(tableFilter == 'SUBSIDY' || tableFilter == 'LEAVE'){
+      request.query(`select * 
+      from BOTFRONT_ALL_${tableFilter}
+      where ${tableFilter}_ID = ${admin_select}`, (err, result) => {
+        if(err){
+          console.log(err)
+          return
+        }
+        const projectCheck = result.recordset
+        if(projectCheck.length == 0){
+          req.flash('error', '無此新增項目，請重新嘗試!!')
+          return res.redirect('/adminSearch/new')
+        }else{
+          request.query(`select * 
+          from BOTFRONT_${tableFilter}_INFO
+          where CPY_NO = ${companyFilter} 
+          and ${tableFilter}_NO = ${admin_select}`, (err, result) => {
+            if(err){
+              console.log(err)
+              return
+            }
+            const projectAdded = result.recordset
+            if(projectAdded.length == 0){
+              request.input('cpy_no', sql.Int, companyFilter)
+              .input('project_no', sql.Int, admin_select)
+              .input('des', sql.NVarChar(2000), adminSearch_des)
+              .query(`insert into BOTFRONT_${tableFilter}_INFO (CPY_NO, ${tableFilter}_NO, ${tableFilter}_DES)
+              values (@cpy_no, @project_no, @des)`, (err, result) => {
+                if(err){
+                  console.log(err)
+                  return
+                }
+                req.flash('success_msg', '回覆新增成功!!')
+                return res.redirect(`/adminSearch/filter?companyFilter=${companyFilter}&tableFilter=${tableFilter}&search=`)
+              })
+            }else{
+              req.flash('error', '無法新增資料，請確認此公司是否已新增過此類別資料，如已新增過請使用編輯功能修改內容!!')
+              return res.redirect('/adminSearch/new')
+            }
+          })
+        }
+      })
+    }
+  })
+})
+
+// API => admin 新增職缺資訊 取得未新增過的職缺類別
+router.get('/api/v1/new/POSITION/:cpy_no/:industry_no', (req, res) => {
+	const {cpy_no, industry_no} = req.params
+
+	const request = new sql.Request(pool)
+
+	// 抓取未新增過的職缺資料
+	request.query(`select a.POSITION_ID, a.POSITION_NAME 
+	from BOTFRONT_ALL_POSITION a 
+	where not exists (select * 
+	from BOTFRONT_POSITION_INFO b 
+	where  a.POSITION_ID = b.POSITION_NO 
+	and b.CPY_NO = ${cpy_no}) 
+	and a.INDUSTRY_NO = ${industry_no}`, (err, result) => {
+		if(err){
+		console.log(err)
+		return
+		}
+
+		return res.send(result)
+	})
+})
+
+// API => admin 抓取未新增過的類別
+router.get('/api/v1/new/:table/:cpy_no', (req, res) => {
+	const {cpy_no, table} = req.params
+
+  const request = new sql.Request(pool)
+  // 抓取公司的產業類別
+  if(table == 'INDUSTRY'){
+    request.query(`select a.INDUSTRY_NO as Id, b.INDUSTRY_NAME as Name
+    from BOTFRONT_USERS_INFO a
+    left join BOTFRONT_TYPE_OF_INDUSTRY b
+    on a.INDUSTRY_NO = b.INDUSTRY_ID
+    where CPY_ID = ${cpy_no}`, (err, result) => {
+      if(err){
+      console.log(err)
+      return
+      }
+
+      return res.send(result)
+    })
+  }else if(table == 'COMPANY_INFO'){// 抓取未新增過的公司資訊類別
+    request.query(`select a.INFO_ID, a.INFO_NAME 
+    from BOTFRONT_ALL_COMPANY_INFO a 
+    where not exists (select * 
+    from BOTFRONT_COMPANY_INFO b 
+    where  a.INFO_ID = b.INFO_NO 
+    and b.CPY_NO = ${cpy_no})`, (err, result) => {
+      if(err){
+      console.log(err)
+      return
+      }
+      
+      return res.send(result)
+    })
+  }else{ // 因為假別、補助、問候、預設回覆的資料庫結構都一樣，所以直接寫成一個
+    request.query(`select a.${table}_ID as Id, a.${table}_NAME  as Name
+    from BOTFRONT_ALL_${table} a 
+    where not exists (select * 
+    from BOTFRONT_${table}_INFO b 
+    where  a.${table}_ID = b.${table}_NO 
+    and b.CPY_NO = ${cpy_no})`, (err, result) => {
+      if(err){
+      console.log(err)
+      return
+      }
+      
+      return res.send(result)
+    })
+  }
+})
+
+// 顯示新增頁面
+router.get('/new', (req, res) => {
+  const request = new sql.Request(pool)
+
+  request.query(`select *
+  from BOTFRONT_USERS_INFO`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const adminCompany = result.recordset
+    res.render('new_adminSearch', {adminCompany})
+  })
+})
+
+// 編輯回覆內容
 router.put('/:cpy_no/:table/:adminSearch_no', (req, res) => {
   const {cpy_no, table, adminSearch_no} = req.params
   const {adminSearch_des} = req.body
@@ -72,7 +329,7 @@ router.put('/:cpy_no/:table/:adminSearch_no', (req, res) => {
             }
             const adminSearchInfo = result.recordset
             const tableFilter = table
-            const companyFilter = adminSearchInfo[0].CPY_NAME
+            const companyFilter = adminSearchInfo[0].CPY_NO
             // console.log(adminSearchInfo)
             if(adminSearchInfo.length == 0){
               warning.push({message: '未查詢到此回覆資訊，請重新查詢!'})
@@ -134,7 +391,7 @@ router.put('/:cpy_no/:table/:adminSearch_no', (req, res) => {
             }
             const adminSearchInfo = result.recordset
             const tableFilter = table
-            const companyFilter = adminSearchInfo[0].CPY_NAME
+            const companyFilter = adminSearchInfo[0].CPY_NO
             // console.log(adminSearchInfo)
             if(adminSearchInfo.length == 0){
               warning.push({message: '未查詢到此回覆資訊，請重新查詢!'})
@@ -149,6 +406,7 @@ router.put('/:cpy_no/:table/:adminSearch_no', (req, res) => {
   }
 })
 
+// 顯示編輯頁面
 router.get('/:cpy_no/:table/:adminSearch_no/edit', (req, res) => {
   const {cpy_no, table, adminSearch_no} = req.params
 
@@ -194,6 +452,7 @@ router.get('/:cpy_no/:table/:adminSearch_no/edit', (req, res) => {
   }
 })
 
+// 篩選公司及類別進行查詢並顯示頁面
 router.get('/filter', (req, res) => {
   const {companyFilter, tableFilter, search} = req.query
 
@@ -235,7 +494,7 @@ router.get('/filter', (req, res) => {
         on a.INFO_NO = b.INFO_ID
         left join BOTFRONT_USERS_INFO c
         on a.CPY_NO = c.CPY_ID
-        where c.CPY_NAME = '${companyFilter}'`, (err, result) => {
+        where c.CPY_ID = '${companyFilter}'`, (err, result) => {
           if(err){
             console.log(err)
             return
@@ -256,7 +515,7 @@ router.get('/filter', (req, res) => {
         on a.${tableFilter}_NO = b.${tableFilter}_ID
         left join BOTFRONT_USERS_INFO c
         on a.CPY_NO = c.CPY_ID
-        where c.CPY_NAME = '${companyFilter}'`, (err, result) => {
+        where c.CPY_ID = '${companyFilter}'`, (err, result) => {
           if(err){
             console.log(err)
             return
@@ -280,7 +539,7 @@ router.get('/filter', (req, res) => {
         on a.INFO_NO = b.INFO_ID
         left join BOTFRONT_USERS_INFO c
         on a.CPY_NO = c.CPY_ID
-        where c.CPY_NAME = '${companyFilter}' and b.INFO_NAME like '%${search}%'`, (err, result) => {
+        where c.CPY_ID = '${companyFilter}' and b.INFO_NAME like '%${search}%'`, (err, result) => {
           if(err){
             console.log(err)
             return
@@ -301,7 +560,7 @@ router.get('/filter', (req, res) => {
         on a.${tableFilter}_NO = b.${tableFilter}_ID
         left join BOTFRONT_USERS_INFO c
         on a.CPY_NO = c.CPY_ID
-        where c.CPY_NAME = '${companyFilter}' and b.${tableFilter}_NAME like '%${search}%'`, (err, result) => {
+        where c.CPY_ID = '${companyFilter}' and b.${tableFilter}_NAME like '%${search}%'`, (err, result) => {
           if(err){
             console.log(err)
             return
@@ -319,6 +578,7 @@ router.get('/filter', (req, res) => {
   })
 })
 
+// 顯示空白查詢頁面
 router.get('/', (req, res) => {
   const request = new sql.Request(pool)
 
