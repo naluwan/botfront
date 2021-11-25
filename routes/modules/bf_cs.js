2616e6492d7ee9006fe324eff993fad4098a3128
naluwn <street.baller06@gmail.com>
2021-11-25 11:35:17 +0800

    feat: add cs_question router and build an API for search funtion by category

--- routes/modules/bf_cs.js
+++ routes/modules/bf_cs.js
@@ -54,6 +54,100 @@ router.get('/function/filter', (req, res) => {
   })
 })
 
+// 查詢類別下的功能 API
+router.get('/api/v1/function/:category_id', (req, res) => {
+  const {category_id} = req.params
+  const request = new sql.Request(pool)
+
+  request.query(`select *
+  from BF_CS_FUNCTION
+  where CATEGORY_ID = ${category_id}`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    return res.send(result)
+  })
+})
+
+router.get('/question/filter', (req, res) => {
+  const {categorySelect, functionSelect, search} = req.query
+  const request = new sql.Request(pool)
+  const warning = []
+  // console.log(functionSelect)
+  if(search && (!categorySelect || !functionSelect)){
+    warning.push({message: '請先選取類別和功能再進行查詢!!'})
+    return res.render('cs_question', {search})
+  }
+
+  request.query(`select * 
+  from BF_CS_CATEGORY`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const categoryInfo = result.recordset
+    request.query(`select * 
+    from BF_CS_FUNCTION
+    where CATEGORY_ID = ${categorySelect}`, (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      const functionInfo = result.recordset
+      if(!search){
+        request.query(`select * 
+        from BF_CS_QUESTION
+        where FUNCTION_ID = ${functionSelect}`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+          const questionInfo = result.recordset
+          if(questionInfo.length == 0) warning.push({message: '查無此功能的問答，請先新增問答!!'})
+          return res.render('cs_question', {categoryInfo, functionInfo, questionInfo, categorySelect, functionSelect, warning})
+        })
+      }else{
+        request.query(`select *
+        from BF_CS_QUESTION
+        where FUNCTION_ID = ${functionSelect}
+        and DESCRIPTION like '%${search}%'`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+          const questionInfo = result.recordset
+          if(questionInfo.length == 0) warning.push({message: '查無此問答，請重新嘗試!!'})
+          return res.render('cs_question', {categoryInfo, functionInfo, questionInfo, categorySelect, functionSelect, warning, search})
+        })
+      }
+    })
+  })
+})
+
+
+
+router.get('/question', (req, res) => {
+  const request = new sql.Request(pool)
+  const warning = []
+
+  request.query(`select *
+  from BF_CS_CATEGORY`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const categoryInfo = result.recordset
+    if(categoryInfo.length == 0){
+      warning.push({message: '查無類別資料，請先新增類別!!'})
+      return res.render('cs_function', {warning})
+    }else{
+      warning.push({message: '請先選擇類別和功能!!'})
+      return res.render('cs_question', {categoryInfo, warning})
+    } 
+  })
+})
+
 router.get('/function', (req, res) => {
   const request = new sql.Request(pool)
   const warning = []
