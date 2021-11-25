f92eda9062ea9bd6aa28b08e5571735d321dc3fe
naluwn <street.baller06@gmail.com>
2021-11-25 12:08:30 +0800

    feat: add comment

--- routes/modules/bf_cs.js
+++ routes/modules/bf_cs.js
@@ -8,16 +8,18 @@ const sql = require('mssql')
 const pool = require('../../config/connectPool')
 const { query } = require('express')
 
+// 顯示功能資料頁面
 router.get('/function/filter', (req, res) => {
   const {category, search} = req.query
   const request = new sql.Request(pool)
   const warning = []
 
+  // 沒有選擇類別就使用關鍵字的錯誤處理
   if(search & !category){
-    warning.push({message: '請先選擇類別再進行查詢!!'})
-    return res.render('cs_function', {search, warning})
+    req.flash('warning_msg', '請先選擇類別再進行查詢!!')
+    return res.redirect('/bf_cs/function')
   }
-  
+  // 抓取類別資料
   request.query(`select *
   from BF_CS_CATEGORY`, (err, result) => {
     if(err){
@@ -25,7 +27,9 @@ router.get('/function/filter', (req, res) => {
       return
     }
     const categoryInfo = result.recordset
+    // 沒有搜尋關鍵字
     if(!search){
+      // 抓取指定類別的功能資料
       request.query(`select * 
       from BF_CS_FUNCTION
       where CATEGORY_ID = ${category}`, (err, result) => {
@@ -37,7 +41,8 @@ router.get('/function/filter', (req, res) => {
         if(functionInfo.length == 0) warning.push({message: '查無此類別資料，請先拉至下方新增功能!!'})
         return res.render('cs_function', {categoryInfo, functionInfo, category, warning})
       })
-    }else{
+    }else{  //有搜尋關鍵字
+      // 抓取指定類別並包含搜尋關鍵字的功能資料
       request.query(`select * 
       from BF_CS_FUNCTION
       where CATEGORY_ID = ${category}
@@ -54,11 +59,12 @@ router.get('/function/filter', (req, res) => {
   })
 })
 
-// 查詢類別下的功能 API
+// 查詢類別下的功能資料 API
 router.get('/api/v1/function/:category_id', (req, res) => {
   const {category_id} = req.params
   const request = new sql.Request(pool)
 
+  // 抓取指定類別的功能資料
   request.query(`select *
   from BF_CS_FUNCTION
   where CATEGORY_ID = ${category_id}`, (err, result) => {
@@ -70,16 +76,18 @@ router.get('/api/v1/function/:category_id', (req, res) => {
   })
 })
 
+// 查詢問答頁面前的篩選
 router.get('/question/filter', (req, res) => {
   const {categorySelect, functionSelect, search} = req.query
   const request = new sql.Request(pool)
   const warning = []
-  // console.log(functionSelect)
+  
+  // 未選擇類別和功能就搜尋的錯誤處理
   if(search && (!categorySelect || !functionSelect)){
-    warning.push({message: '請先選取類別和功能再進行查詢!!'})
-    return res.render('cs_question', {search})
+    req.flash('warning_msg', '請先選擇類別和功能再進行查詢!!')
+    return res.redirect('/bf_cs/question')
   }
-
+  // 抓取類別資料
   request.query(`select * 
   from BF_CS_CATEGORY`, (err, result) => {
     if(err){
@@ -87,6 +95,7 @@ router.get('/question/filter', (req, res) => {
       return
     }
     const categoryInfo = result.recordset
+    // 抓取指定類別的功能資料
     request.query(`select * 
     from BF_CS_FUNCTION
     where CATEGORY_ID = ${categorySelect}`, (err, result) => {
@@ -95,7 +104,9 @@ router.get('/question/filter', (req, res) => {
         return
       }
       const functionInfo = result.recordset
+      // 如果沒有搜尋關鍵字
       if(!search){
+        // 抓取指定功能的問答資料
         request.query(`select * 
         from BF_CS_QUESTION
         where FUNCTION_ID = ${functionSelect}`, (err, result) => {
@@ -107,7 +118,8 @@ router.get('/question/filter', (req, res) => {
           if(questionInfo.length == 0) warning.push({message: '查無此功能的問答，請先新增問答!!'})
           return res.render('cs_question', {categoryInfo, functionInfo, questionInfo, categorySelect, functionSelect, warning})
         })
-      }else{
+      }else{  // 有搜尋關鍵字
+        // 抓取指定功能包含搜尋關鍵字的問答資料
         request.query(`select *
         from BF_CS_QUESTION
         where FUNCTION_ID = ${functionSelect}
@@ -125,12 +137,12 @@ router.get('/question/filter', (req, res) => {
   })
 })
 
-
-
+// 顯示問答頁面
 router.get('/question', (req, res) => {
   const request = new sql.Request(pool)
   const warning = []
 
+  // 抓取類別資料
   request.query(`select *
   from BF_CS_CATEGORY`, (err, result) => {
     if(err){
@@ -148,10 +160,12 @@ router.get('/question', (req, res) => {
   })
 })
 
+// 顯示功能頁面
 router.get('/function', (req, res) => {
   const request = new sql.Request(pool)
   const warning = []
   
+  // 抓取類別資料
   request.query(`select *
   from BF_CS_CATEGORY`, (err, result) => {
     if(err){
