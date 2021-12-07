85423bf5869a8978162bda7944bca7409623b110
naluwn <street.baller06@gmail.com>
2021-12-07 12:10:00 +0800

    feat: try to use rasa http api

--- routes/modules/train.js
+++ routes/modules/train.js
@@ -4,29 +4,59 @@ const request = require('request')
 const yaml = require('js-yaml')
 const fs = require('fs')
 
-router.get('/', (req, res) => {
-
-  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.txt', 'utf8'))
+router.post('/', (req, res) => {
+  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.json', 'utf8'))
   const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
   const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
-
+  const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
   let nlu = yaml.dump(nluData)
   let domain = yaml.dump(domainData)
   let config = yaml.dump(configData)
+  let fragments = yaml.dump(fragmentsData)
 
+  const zh = nluData
+  let data = {
+    "config":{config},
+    "nlu": {zh},
+    "domain": domain,
+    "fragments": fragments,
+  }
+
+  let url = 'http://192.168.10.108:5005/model/train?save_to_default_model_directory=true&force_training=true&augmentation=50&num_threads=1'
+  req.header['content-type'] =  "application/x-yaml"
+  
+
+  req.pipe(request(url)).pipe(res)
+})
+
+router.get('/trainComplate', (req, res) => {
+  req.flash('success_msg', '訓練完成!!')
+  return res.redirect('/')
+})
+
+router.get('/', (req, res) => {
+  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.json', 'utf8'))
+  const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
+  const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
+  const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
+  let nlu = yaml.dump(nluData)
+  let domain = yaml.dump(domainData)
+  let config = yaml.dump(configData)
+  let fragments = yaml.dump(fragmentsData)
   // nlu = JSON.stringify(nlu)
   // domain = JSON.stringify(domainData)
   // config = JSON.stringify(configData)
   // zh = JSON.stringify(zh)
-  const zh = nluData.nlu
+  const zh = nluData
   // config = JSON.stringify({config})
   // console.log(nluDataJson)
   // console.log(config)
   // console.log(configData2)
   let data = {
-    config:{config},
-    nlu: {zh},
-    domain: domain,
+    "config":{config},
+    "nlu": {zh},
+    "domain": domain,
+    "fragments": fragments,
   }
 
 // data = JSON.stringify(data)
@@ -43,10 +73,11 @@ router.get('/', (req, res) => {
     body: data
   }, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
+      req.flash('success_msg', '訓練完成!!')
     }
   })
-  // console.log(request.payload)
+  req.flash('warning_msg', '訓練中.......')
+      return res.redirect('/')
 })
 
 
