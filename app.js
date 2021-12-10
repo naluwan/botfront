8c46addc3add115ab034fbbb33f89f2070db6f43
naluwn <street.baller06@gmail.com>
2021-12-10 11:49:01 +0800

    feaet: set args of res.local

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-
-data = {
-  "config": "language: zh\npipeline:\n- name: DucklingEntityExtractor\nurl: \"http://duckling:8000\"\ntimezone: Asia/Taipei\nlocale: zh_TW\ndimensions:\n- date\n- email\n- name: CountVectorsFeaturizer\n- name: CountVectorsFeaturizer\nanalyzer: \"char_wb\"\nmin_ngram: 1\nmax_ngram: 4\n- name: DIETClassifier\nepochs: 200\nconstrain_similarities: true\nmodel_confidence: linear_norm\n- name: rasa_addons.nlu.components.gazette.Gazette\n- name: >-\nrasa_addons.nlu.components.intent_ranking_canonical_example_injector.IntentRankingCanonicalExampleInjector\n-name: EntitySynonymMapper\n- name: ResponseSelector\nepochs: 100\nconstrain_similarities: true\n- name: FallbackClassifier\nthreshold: 0.7\nambiguity_threshold: 0.1\n- name: ResponseSelector\nepochs: 100\nretrieval_intent: faq\n- name: ResponseSelector\nepochs: 100\nretrieval_intent: chitchat"
-}
-
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 require('./config/connectPool')
@@ -46,6 +41,7 @@ app.use((req, res, next) => {
   if(res.locals.isAuthenticated){
     res.locals.isAdmin = user.ISADMIN
     res.locals.isHr = user.ISHR
+    res.locals.whichRole = user.WHICH_ROLE
   }
   res.locals.warning_msg = req.flash('warning_msg')
   res.locals.success_msg = req.flash('success_msg')
