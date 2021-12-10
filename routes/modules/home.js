bc9ec13333e68ac21c5b596ff82fee3d98a7bb3c
naluwn <street.baller06@gmail.com>
2021-12-10 12:23:53 +0800

    feat: 新增顯示admin頁面 未訓練客服資訊 router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -23,20 +23,48 @@ router.put('/roleFilter', (req, res) => {
 
 router.get('/', (req, res) => {
 	const isAdmin = res.locals.isAdmin
+	const whichRole = res.locals.whichRole
 	if(isAdmin){
 		const request = new sql.Request(pool)
-		request.query(`select *
-		from BOTFRONT_ALL_POSITION
-		where TRAINED = 0 or HAD_READ = 0`, (err, result) => {
-			if(err){
-				console.log(err)
-				return
-			}
-			const position = result.recordset
-			const notTrained = position.filter(item => item.TRAINED == 0)
-			const notRead = position.filter(item => item.HAD_READ == 0)
-			return res.render('index', {notTrained: notTrained.length, notRead: notRead.length})
-		})
+		if(whichRole == 'hire'){
+			request.query(`select *
+			from BOTFRONT_ALL_POSITION
+			where TRAINED = 0 or HAD_READ = 0`, (err, result) => {
+				if(err){
+					console.log(err)
+					return
+				}
+				const position = result.recordset
+				const notTrained = position.filter(item => item.TRAINED == 0)
+				const notRead = position.filter(item => item.HAD_READ == 0)
+				return res.render('index', {notTrained: notTrained.length, notRead: notRead.length})
+			})
+		}
+
+		if(whichRole == 'hr'){
+			request.query(`select *
+			from BF_CS_FUNCTION
+			where TRAINED = 0`, (err, result) => {
+				if(err){
+					console.log(err)
+					return
+				}
+				const functionInfo = result.recordset
+				const trainFunction = functionInfo.filter(item => item.TRAINED == 0)
+
+				request.query(`select *
+				from BF_CS_QUESTION
+				where TRAINED = 0`, (err, result) => {
+					if(err){
+						console.log(err)
+						return
+					}
+					const questionInfo = result.recordset
+					const trainQuestion = questionInfo.filter(item => item.TRAINED == 0)
+					return res.render('index', {trainFunction: trainFunction.length, trainQuestion: trainQuestion.length})
+				})
+			})
+		}
 	}else{
 		res.render('index')
 	}
