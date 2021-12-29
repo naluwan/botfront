3c4ca1a048eca6b3f110c22eee70ace5b0d57ed6
naluwn <street.baller06@gmail.com>
2021-12-29 12:15:33 +0800

    refactor: 重構train router,將train router做成API返回訓練資料

--- routes/modules/train.js
+++ routes/modules/train.js
@@ -1,84 +1,132 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
 const yaml = require('js-yaml')
 const fs = require('fs')
+const axios = require('axios')
 
-router.post('/', (req, res) => {
-  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.json', 'utf8'))
-  const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
-  const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
-  const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
-  let nlu = yaml.dump(nluData)
-  let domain = yaml.dump(domainData)
-  let config = yaml.dump(configData)
-  let fragments = yaml.dump(fragmentsData)
-
-  const zh = nluData
-  let data = {
-    "config":{config},
-    "nlu": {zh},
-    "domain": domain,
-    "fragments": fragments,
-  }
-
-  let url = 'http://192.168.10.108:5005/model/train?save_to_default_model_directory=true&force_training=true&augmentation=50&num_threads=1'
-  req.header['content-type'] =  "application/x-yaml"
-  
-
-  req.pipe(request(url)).pipe(res)
-})
-
-router.get('/trainComplate', (req, res) => {
-  req.flash('success_msg', '訓練完成!!')
-  return res.redirect('/')
-})
-
+// 點擊按鈕，呼叫訓練router，提示訓練中
 router.get('/', (req, res) => {
-  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.json', 'utf8'))
+  // 載入training data
+  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json-2.json', 'utf8'))
   const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
   const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
   const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
+
+  // 轉換格式
   let nlu = yaml.dump(nluData)
   let domain = yaml.dump(domainData)
   let config = yaml.dump(configData)
   let fragments = yaml.dump(fragmentsData)
-  // nlu = JSON.stringify(nlu)
-  // domain = JSON.stringify(domainData)
-  // config = JSON.stringify(configData)
-  // zh = JSON.stringify(zh)
   const zh = nluData
-  // config = JSON.stringify({config})
-  // console.log(nluDataJson)
-  // console.log(config)
-  // console.log(configData2)
+
+  // newContent = {
+  //   "text": "我想知道中鋼的現價",
+  //   "intent": "問股票",
+  //   "entities": [
+  //     { "entity": "company", "value": "中鋼", "start": 4, "end": 6 },
+  //     { "entity": "stock", "value": "現價", "start": 7, "end": 9 }
+  //   ],
+  //   "metadata": { "language": "zh", "canonical": true }
+  // }
+
+  // const arrayText = []
+  // zh.rasa_nlu_data.common_examples.forEach(item => {
+  //   arrayText.push(item.text)
+  // })
+  // console.log(arrayText)
+  // const index = arrayText.indexOf(newContent.text)
+  // console.log(index)
+  // zh.rasa_nlu_data.common_examples.splice(index,1)
+  // console.log(zh.rasa_nlu_data.common_examples)
+
+  // const repeatText = zh.rasa_nlu_data.common_examples.filter(item => item.text == newContent.text)
+  
+  // if(repeatText.length){
+  //   console.log(repeatText)
+  //   console.log('資料重複')
+  // }else{
+  //   zh.rasa_nlu_data.common_examples.push(newContent)
+  //   console.log(zh.rasa_nlu_data.common_examples)
+    // try{
+    //   fs.writeFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json-1.json', JSON.stringify(zh))
+    // } catch(err){
+    //   console.log(err)
+    // }
+  //   console.log('新增資料')
+  // }
+  
   let data = {
-    "config":{config},
-    "nlu": {zh},
-    "domain": domain,
-    "fragments": fragments,
+    'config': {config},
+    'nlu': {zh},
+    'domain': domain,
+    'fragments': fragments,
+    'fixed_model_name': 'model-97090920',
+    'load_model_after': true
   }
 
-// data = JSON.stringify(data)
-// console.log(data)
-// console.log(data["config"]["language"])
-
-  request({
-    url: 'http://192.168.10.108:5005/model/train?save_to_default_model_directory=true&force_training=false&augmentation=50&num_threads=1',
-    method: "POST",
-    json: true,
-    headers: {
-        "content-type": "application/json",
-    },
-    body: data
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      req.flash('success_msg', '訓練完成!!')
-    }
-  })
-  req.flash('warning_msg', '訓練中.......')
-      return res.redirect('/')
+  return res.json(data)
 })
 
+// // 載入training data，向rasa發送訓練資料
+// router.get('/sendData', (req, res, next) => {
+//   const user = res.locals.user
+//   const isAuthenticated = res.locals.isAuthenticated
+//   // 載入training data
+//   const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json.json', 'utf8'))
+//   const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
+//   const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
+//   const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
+
+//   // 轉換格式
+//   let nlu = yaml.dump(nluData)
+//   let domain = yaml.dump(domainData)
+//   let config = yaml.dump(configData)
+//   let fragments = yaml.dump(fragmentsData)
+//   const zh = nluData
+
+//   // request body
+//   let data = {
+//     'config': {config},
+//     'nlu': {zh},
+//     'domain': domain,
+//     'fragments': fragments,
+//     'fixed_model_name': 'model-97090920',
+//     'load_model_after': true
+//   }
+
+  // 發送請求
+//   axios({
+//     method: 'post',
+//     url: 'http://192.168.10.108:5005/model/train',
+//     json: true,
+//     headers: {
+//       "content-type": "application/json",
+//     },
+//     data: data,
+//     params: {
+//       save_to_default_model_directory: true,
+//       force_training: false,
+//     }
+//   }).then((response) => {
+//     console.log('訓練完成')
+//     req.flash('success_msg', '訓練完成')
+//     return res.redirect('/')
+//   })
+//   .catch((err) => console.log('sendData錯誤： ' + err))
+//   res.send('傳送資料')
+// })
+
+// router.post('/complete', (req, res) => {
+//   console.log('訓練完成')
+//   req.flash('success_mg', '訓練完成!!')
+//   console.log(req.session)
+//   return res.redirect('/')
+// })
+
+// router.get('/time', (req, res, next) => {
+//   console.log('訓練中.......')
+//   req.flash('warning_msg', '訓練中.......')
+//   res.redirect('/')
+// })
 
 module.exports = router
