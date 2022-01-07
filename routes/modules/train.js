4c10ad43d818633919dc375f84f32d59271dd564
naluwn <street.baller06@gmail.com>
2022-01-07 14:06:58 +0800

    feat: 新增抓取訓練資料router

--- routes/modules/train.js
+++ routes/modules/train.js
@@ -3,14 +3,17 @@ const router = express.Router()
 const yaml = require('js-yaml')
 const fs = require('fs')
 const axios = require('axios')
+const path = require('path')
+const sql = require('mssql')
+const pool = require('../../config/connectPool')
 
 // 點擊按鈕，呼叫訓練router，提示訓練中
 router.get('/', (req, res) => {
   // 載入training data
-  const nluData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/nlu-json-3.json', 'utf8'))
-  const configData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/config.yml', "utf8"))
-  const domainData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/domain.yml', 'utf8'))
-  const fragmentsData = yaml.load(fs.readFileSync('C:/Users/AN1100275/Desktop/training-data/data/stories/fragments.yml', 'utf8'))
+  const nluData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/nlu-json.json'), 'utf8'))
+  const configData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/config.yml'), "utf8"))
+  const domainData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/domain.yml'), 'utf8'))
+  const fragmentsData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/fragments.yml'), 'utf8'))
 
   // 轉換格式
   let nlu = yaml.dump(nluData)
@@ -63,8 +66,83 @@ router.get('/', (req, res) => {
     'fixed_model_name': 'model-97090920',
     'load_model_after': true
   }
-
+  // console.log(data)
   return res.json(data)
+})
+
+router.get('/trainingData', (req, res) =>{
+  const request = new sql.Request(pool)
+
+  request.query(`select DATA_CONTENT as config
+  from BF_CS_TRAINING_DATA
+  where DATA_NAME = 'config'`, (err, result) => {
+    if(err){
+      console.log(err)
+      return
+    }
+    const config = result.recordset[0]['config']
+    
+    request.query(`select DATA_CONTENT as domain
+    from BF_CS_TRAINING_DATA
+    where DATA_NAME = 'domain'` , (err, result) => {
+      if(err){
+        console.log(err)
+        return
+      }
+      const domain = result.recordset[0]['domain']
+
+      request.query(`select DATA_CONTENT as fragments
+      from BF_CS_TRAINING_DATA
+      where DATA_NAME = 'fragments'`, (err, result) => {
+        if(err){
+          console.log(err)
+          return
+        }
+        const fragments = result.recordset[0]['fragments']
+
+        request.query(`select DATA_CONTENT as nlu
+        from BF_CS_TRAINING_DATA
+        where DATA_NAME = 'nlu-json'`, (err, result) => {
+          if(err){
+            console.log(err)
+            return
+          }
+          const nluJson = result.recordset[0]['nlu']
+          // console.log(result)
+          try{
+            fs.writeFileSync(path.resolve(__dirname, '../../public/trainData/config.yml'), config)
+            fs.writeFileSync(path.resolve(__dirname, '../../public/trainData/domain.yml'), domain)
+            fs.writeFileSync(path.resolve(__dirname, '../../public/trainData/fragments.yml'), fragments)
+            fs.writeFileSync(path.resolve(__dirname, '../../public/trainData/nlu-json.json'), nluJson)
+          } catch(err){
+            console.log(err)
+          }
+          
+          const nluData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/nlu-json.json'), 'utf8'))
+          const configData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/config.yml'), "utf8"))
+          const domainData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/domain.yml'), 'utf8'))
+          const fragmentsData = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../public/trainData/fragments.yml'), 'utf8'))
+
+          // 轉換格式
+          const domainYml = yaml.dump(domainData)
+          const configYml = yaml.dump(configData)
+          const fragmentsYml = yaml.dump(fragmentsData)
+          const zh = nluData
+          // console.log(zh)
+          let data = {
+            'config': {configYml},
+            'nlu': {zh},
+            'domain': domainYml,
+            'fragments': fragmentsYml,
+            'fixed_model_name': 'model-97090920',
+            'load_model_after': true
+          }
+          // console.log(data)
+          return res.json(data)
+        })
+      })
+    })
+  })
 })
 
 // // 載入training data，向rasa發送訓練資料
@@ -94,7 +172,7 @@ router.get('/', (req, res) => {
 //     'load_model_after': true
 //   }
 
-  // 發送請求
+//   // 發送請求
 //   axios({
 //     method: 'post',
 //     url: 'http://192.168.10.108:5005/model/train',
